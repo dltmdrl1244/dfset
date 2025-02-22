b971f7001717933758953e2559653a27d4e51b3f
SeungGi Lee <dltmdrl1244@naver.com>
2025-02-22 16:32:58 +0900

    implicit type 수정 : hexToRGB

--- app/components/itemTable.tsx
+++ app/components/itemTable.tsx
@@ -23,7 +23,7 @@ interface ItemTableProps {
   isAdventure: boolean;
 }
 
-const hexToRGB = (hex, opacity: number): string => {
+const hexToRGB = (hex: string, opacity: number): string => {
   const r = parseInt(hex.slice(1, 3), 16);
   const g = parseInt(hex.slice(3, 5), 16);
   const b = parseInt(hex.slice(5, 7), 16);
