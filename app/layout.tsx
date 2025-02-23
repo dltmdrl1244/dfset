0eda6790ab9db78538ffe1e9c7dad4cefb280170
SeungGi Lee <dltmdrl1244@naver.com>
2025-02-23 16:06:46 +0900

    vercel analytics 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Noto_Sans_KR } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "DFset 던파셋 - 던전앤파이터 아이템 기록 검색",
