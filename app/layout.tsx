083b3a7f76200a7598551a84624436a535613c07
SeungGi Lee <dltmdrl1244@naver.com>
2025-02-23 15:34:01 +0900

    탭 이름 변경, 레거시 파일 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Noto_Sans_KR } from "next/font/google";
 
 export const metadata: Metadata = {
-  title: "DFset - 던전앤파이터 아이템 기록 검색",
+  title: "DFset 던파셋 - 던전앤파이터 아이템 기록 검색",
   description: "DFset",
 };
 
