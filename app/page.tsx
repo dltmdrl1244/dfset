ab033579254f5c2fcf885ed241417e05b1ad9dcb
SeungGi Lee <dltmdrl1244@naver.com>
2025-02-26 01:05:14 +0900

    [임시] 공지사항 추가

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,16 @@ import Title from "./components/title";
 import Form from "./components/form";
 import SearchResult from "./components/characterSearchResult";
 import { useState } from "react";
-import { VStack } from "@chakra-ui/react";
+import {
+  HStack,
+  ListItem,
+  Text,
+  UnorderedList,
+  VStack,
+} from "@chakra-ui/react";
 import BaseLayout from "./components/baseLayout";
 import SearchHistory from "./components/searchHistory";
+import { InfoIcon } from "@chakra-ui/icons";
 
 export default function Home() {
   const [searchResult, setSearchResult] = useState<Character[]>([]);
@@ -34,6 +41,23 @@ export default function Home() {
         />
       </VStack>
       <SearchResult searchResult={searchResult} />
+
+      <HStack
+        width="500px"
+        border="1px solid dimgray"
+        p={4}
+        borderRadius="md"
+        gap={3}>
+        <InfoIcon />
+        <VStack>
+          <Text fontSize={"sm"}>
+            2025-02-26 01:00부로 DB 구조가 업데이트되었습니다. <br></br>
+            캐릭터별로 검색을 새로 해주셔야 모험단 전체 테이블에 정보가
+            반영됩니다. <br></br>
+            이용에 불편을 드려 죄송합니다.
+          </Text>
+        </VStack>
+      </HStack>
     </BaseLayout>
   );
 }
