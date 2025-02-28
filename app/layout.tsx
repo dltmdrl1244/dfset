af112b56dbe0dd61de9590c13a6614fba94d3620
SeungGi Lee <dltmdrl1244@naver.com>
2025-03-01 03:30:30 +0900

    adsense 삽입

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import { Analytics } from "@vercel/analytics/react";
 export const metadata: Metadata = {
   title: "DFset 던파셋 - 던전앤파이터 아이템 기록 검색",
   description: "DFset",
+  other: {
+    "google-adsense-account": "ca-pub-9624261648784529",
+  },
 };
 
 const notoSansKr = Noto_Sans_KR({
