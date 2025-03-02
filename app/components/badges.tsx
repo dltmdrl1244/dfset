83f53fa9285569832f8901d89737a8dd8e93ed95
SeungGi Lee <dltmdrl1244@naver.com>
2025-03-03 02:22:34 +0900

    itemTable 세트포인트 조회 기능 추가

--- app/components/badges.tsx
+++ app/components/badges.tsx
@@ -1,15 +1,59 @@
-import { Badge } from "@chakra-ui/react";
+import { Badge, Text } from "@chakra-ui/react";
 import { serverOptions } from "../context/serverOptions";
 
 interface ServerBadgeProps {
   serverId: string;
 }
 
+interface SetPointBadgeProps {
+  point: number;
+}
+
+const rareSetPoint = 750;
+const uniqueSetPoint = 1200;
+const legendarySetPoint = 1650;
+const epicSetPoint = 2100;
+const taechoSetPoint = 2550;
+const setPointStep = 80;
+const romeText = ["I", "II", "III", "IV", "V", "V"];
+
 export const ServerBadge: React.FC<ServerBadgeProps> = ({ serverId }) => {
   const server = serverOptions.find((server) => server.serverId === serverId);
   return (
     <Badge bgColor={"gray"} variant="solid" fontSize="sm">
       {server?.serverName}
+    </Badge>
+  );
+};
+
+export const RareBadge = () => {
+  return (
+    <Badge bgColor={"#B36BFF"} variant="solid" fontSize="sm">
+      레어
+    </Badge>
+  );
+};
+
+export const RareTextBadge: React.FC<BadgeTextProps> = ({ str }) => {
+  return (
+    <Badge bgColor={"#B36BFF"} variant="solid" fontSize="sm" color={"black"}>
+      {str}
+    </Badge>
+  );
+};
+
+export const UniqueBadge = () => {
+  return (
+    <Badge bgColor={"#FF00FF"} variant="solid" fontSize="sm">
+      유니크
+    </Badge>
+  );
+};
+
+export const UniqueTextBadge: React.FC<BadgeTextProps> = ({ str }) => {
+  return (
+    <Badge bgColor={"#FF00FF"} variant="solid" fontSize="sm" color={"black"}>
+      {str}
     </Badge>
   );
 };
@@ -22,10 +66,26 @@ export const LegendaryBadge = () => {
   );
 };
 
+export const LegendaryTextBadge: React.FC<BadgeTextProps> = ({ str }) => {
+  return (
+    <Badge bgColor={"#b25400"} variant="solid" fontSize="sm" color={"black"}>
+      {str}
+    </Badge>
+  );
+};
+
 export const EpicBadge = () => {
   return (
     <Badge bgColor={"#FFB400"} variant="solid" fontSize="sm">
       에픽
+    </Badge>
+  );
+};
+
+export const EpicTextBadge: React.FC<BadgeTextProps> = ({ str }) => {
+  return (
+    <Badge bgColor={"#FFB400"} variant="solid" fontSize="sm" color={"black"}>
+      {str}
     </Badge>
   );
 };
@@ -39,6 +99,48 @@ export const TaechoBadge = () => {
       태초
     </Badge>
   );
+};
+
+export const TaechoTextBadge: React.FC<BadgeTextProps> = ({ str }) => {
+  return (
+    <Badge
+      variant="solid"
+      fontSize="sm"
+      bgImage={`linear-gradient(180deg, #28d931, #33acea)`}
+      color={"black"}>
+      {str}
+    </Badge>
+  );
+};
+
+export const SetPointBadge: React.FC<SetPointBadgeProps> = ({ point }) => {
+  if (rareSetPoint <= point && point < uniqueSetPoint) {
+    const tempPoint = point - rareSetPoint;
+    return (
+      <RareTextBadge str={romeText[Math.floor(tempPoint / setPointStep)]} />
+    );
+  } else if (uniqueSetPoint <= point && point < legendarySetPoint) {
+    const tempPoint = point - uniqueSetPoint;
+    return (
+      <UniqueTextBadge str={romeText[Math.floor(tempPoint / setPointStep)]} />
+    );
+  } else if (legendarySetPoint <= point && point < epicSetPoint) {
+    const tempPoint = point - legendarySetPoint;
+    return (
+      <LegendaryTextBadge
+        str={romeText[Math.floor(tempPoint / setPointStep)]}
+      />
+    );
+  } else if (epicSetPoint <= point && point < taechoSetPoint) {
+    const tempPoint = point - epicSetPoint;
+    return (
+      <EpicTextBadge str={romeText[Math.floor(tempPoint / setPointStep)]} />
+    );
+  } else if (taechoSetPoint <= point) {
+    return <TaechoTextBadge str={"I"} />;
+  } else {
+    return <Text>{point}</Text>;
+  }
 };
 
 interface BadgeTextProps {
--- app/components/itemTable.tsx
+++ app/components/itemTable.tsx
@@ -13,10 +13,19 @@ import {
   Center,
   Tooltip,
   Text,
+  HStack,
 } from "@chakra-ui/react";
 import { ItemIcon } from "./itemIcon";
 import { itemSlots } from "../context/slotIds";
 import { itemSets } from "../context/setIds";
+import {
+  EpicTextBadge,
+  LegendaryBadge,
+  LegendaryTextBadge,
+  SetPointBadge,
+  TaechoTextBadge,
+} from "./badges";
+import { useEffect, useState } from "react";
 
 interface ItemTableProps {
   isAdventure: boolean;
@@ -30,10 +39,70 @@ const hexToRGB = (hex: string, opacity: number): string => {
   return `rgba(${r}, ${g}, ${b}, ${opacity})`;
 };
 
+type ItemCounts = [number, number, number];
+type HighestSet = {
+  setIdx: number;
+  setPoint: number;
+};
+const raritySetPoints = [165, 215, 265];
+const uniqueSetPoint = 115;
+
 export const ItemTable: React.FC<ItemTableProps> = ({
   isAdventure,
   adventureHistory,
 }) => {
+  const [points, setPoints] = useState<Record<number, number>>({});
+  const [counts, setCounts] = useState<Record<number, ItemCounts>>({});
+  const [highestSet, setHighestSet] = useState<HighestSet>({
+    setIdx: 0,
+    setPoint: -1,
+  });
+  function calculateSet() {
+    let tempCounts: Record<number, ItemCounts> = {};
+    for (const [key, value] of Object.entries(adventureHistory.highest)) {
+      const setIdx = Math.floor(Number(key) / itemSets.length);
+      const rarity = value.rarity;
+
+      if (setIdx in tempCounts) {
+        tempCounts[setIdx][rarity]++;
+      } else {
+        tempCounts[setIdx] = [0, 0, 0];
+        tempCounts[setIdx][rarity]++;
+      }
+    }
+
+    let tempPoints: Record<number, number> = {};
+    let tempHighestSet: HighestSet = { setIdx: -1, setPoint: -1 };
+    for (const [key, value] of Object.entries(tempCounts)) {
+      const setIdx = Number(key);
+      if (setIdx in tempCounts) {
+        const itemCount =
+          tempCounts[setIdx][0] + tempCounts[setIdx][1] + tempCounts[setIdx][2];
+        tempPoints[setIdx] =
+          raritySetPoints[0] * tempCounts[setIdx][0] +
+          raritySetPoints[1] * tempCounts[setIdx][1] +
+          raritySetPoints[2] * tempCounts[setIdx][2] +
+          uniqueSetPoint * (itemSlots.length - itemCount);
+
+        if (tempHighestSet.setPoint < tempPoints[setIdx]) {
+          tempHighestSet.setIdx = setIdx;
+          tempHighestSet.setPoint = tempPoints[setIdx];
+        }
+      }
+    }
+
+    setPoints(tempPoints);
+    setCounts(tempCounts);
+    setHighestSet(tempHighestSet);
+  }
+
+  useEffect(() => {
+    if (!isAdventure) {
+      calculateSet();
+    }
+  }, [adventureHistory]);
+  // const rarityCount
+
   return (
     <Box maxWidth="730px" boxShadow="base">
       <TableContainer>
@@ -69,6 +138,14 @@ export const ItemTable: React.FC<ItemTableProps> = ({
                   </Center>
                 </Th>
               ))}
+              {!isAdventure && (
+                <Th
+                  border={"1px solid black"}
+                  borderRight={`2px solid black`}
+                  bg="white">
+                  <Text color="black">합계</Text>
+                </Th>
+              )}
             </Tr>
           </Thead>
           <Tbody>
@@ -115,6 +192,57 @@ export const ItemTable: React.FC<ItemTableProps> = ({
                     </Td>
                   );
                 })}
+                {!isAdventure && (
+                  <Td
+                    textAlign="center"
+                    border={"1px solid black"}
+                    p={0}
+                    bg={`${
+                      highestSet.setIdx === setIdx
+                        ? "rgba(255, 0, 0, 0.4)}"
+                        : "rgba(0, 0, 0, 0.2)}"
+                    }`}>
+                    <Tooltip label="빈 슬롯은 유니크로 간주됩니다.">
+                      {setIdx != 12 ? (
+                        <Center flexDirection={"column"} gap={0.5}>
+                          <HStack gap={0.5}>
+                            <LegendaryTextBadge
+                              str={
+                                setIdx in counts
+                                  ? counts[setIdx][0].toString()
+                                  : "0"
+                              }
+                            />
+                            <EpicTextBadge
+                              str={
+                                setIdx in counts
+                                  ? counts[setIdx][1].toString()
+                                  : "0"
+                              }
+                            />
+                            <TaechoTextBadge
+                              str={
+                                setIdx in counts
+                                  ? counts[setIdx][2].toString()
+                                  : "0"
+                              }
+                            />
+                          </HStack>
+                          <HStack gap={0.5}>
+                            <Text>
+                              {setIdx in points ? points[setIdx] : "0"}
+                            </Text>
+                            {setIdx in points && (
+                              <SetPointBadge point={points[setIdx]} />
+                            )}
+                          </HStack>
+                        </Center>
+                      ) : (
+                        <></>
+                      )}
+                    </Tooltip>
+                  </Td>
+                )}
               </Tr>
             ))}
           </Tbody>
