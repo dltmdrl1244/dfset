84f67c1a3b6d45f1f5f8200a9cafdf7b339d66a2
SeungGi Lee <dltmdrl1244@naver.com>
2025-03-17 21:27:33 +0900

    Google adsense 컴포넌트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Noto_Sans_KR } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { GoogleAdSense } from "./components/googleAdSense";
 
 export const metadata: Metadata = {
   title: "DFset 던파셋 - 던전앤파이터 아이템 기록 검색",
@@ -23,12 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html suppressHydrationWarning>
-      <head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9624261648784529"
-          crossOrigin="anonymous"></script>
-      </head>
+      <GoogleAdSense />
       <body className={notoSansKr.className}>
         <Analytics />
         <ChakraProvider>{children}</ChakraProvider>
